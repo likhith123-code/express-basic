20d483189ef9543975026973ff3a66503007c33d
unknown <likhith2901@gmail.com>
2020-08-17 16:23:47 +0530

    first commit

--- index.js
+++ index.js
@@ -1,57 +1,20 @@
-const http = require('http');
-const hostname = 'localhost';
-const fs=require('fs') //file system for getting files
-const path=require('path') //for setting the path
+const express=require('express') //getting express
+const hostname="localhost"
 const port = 3000;
-const server = http.createServer((req,res)=>{
-    //req.url gives url of request
-    console.log("request for the html page whose url is : "+req.url+" and method is : "+req.method);
-    if(req.method == 'GET'){
-        var fileurl;
-        //if deafult host name is entered we serve index html file
-        if(req.url=='/'){
-            fileurl = '/index.html';
-        }
-        //if any other is entered we serve that file
-        else{
-            fileurl=req.url;
-        }
-        var filepath= path.resolve('./public'+fileurl); //creates full path
-        const filext = path.extname(filepath);
-        if(filext=='.html'){
-            //checking if file existed
-            fs.exists(filepath,(exists)=>{
-                if(!exists){
-                    res.statusCode=404;
-                    res.setHeader('type','text/html');
-                    res.end('<html><body>Error 404 file not found </body></html>')
-                    return;
-                }
-                    res.statusCode=200;
-                    res.setHeader('type','text/html');
-                    fs.createReadStream(filepath).pipe(res);
-                
-            })
-        }
-        else{
-            res.statusCode=404;
-            res.setHeader('type','text/html');
-            res.end('<html><body>Incorrect extension not an html page</body></html>');
-            return;
-        }
-    }
-    else{
-        res.statusCode=404;
-        res.setHeader('type','text/html');
-        res.end('<html><body>Incorrect method of calling</body></html>');
-
-    }
-
-})
-
-// server.listen(port,hostname,function_name)       starts litening incoming requests
-
+const morgan=require('morgan');
+const http=require('http') //using http 
+const app=express(); //create new express server
+app.use(morgan('dev')); //use morgan and in it dev type
+app.use(express.static(__dirname+'/public'));  
+// it will serve static pages : first page is index if we change we get other pages.
+//if any wrong page name entered it will display default one i.e. This is express server
+app.use((req,res,next)=>{
+    console.log(req.headers);
+    res.statusCode=200;
+    res.setHeader('type','html/text');
+    res.end('<html><body>This is express server</body></html>');
+});
+const server=http.createServer(app);
 server.listen(port,hostname,()=>{
-    //use back quote only
-    console.log(`Server running at http://${hostname}:${port}`)
+    console.log(`server running at http://${hostname}:${port}`);
 })
